eae193a7a0240f4a5aea5b8cb06d0a5c5ec1f3ca
FabianRaja <fabiraja21052002@gmail.com>
2023-12-10 17:04:03 +0530

    error handled

--- src/Pages/AddPage.jsx
+++ src/Pages/AddPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import WorkSpace from "../Components/Workspace";
 import AddBar from "../Components/AddBar";
+import WorkSpace from "../Components/Workspace";
+
+
 
 export default function AddPage(){
     return(
         //addbar component is used as a children of a workspace
-        <WorkSpace>
-            <AddBar/>
-        </WorkSpace>
+       <WorkSpace>
+        <AddBar/>
+       </WorkSpace>
     )
 }
